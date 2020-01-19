1bec26f1fd87c1562de086ffccde5398c7964ca3
@huangxin <huangxin@gosun.com>
2020-01-19 14:07:28 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box">
+    {{ msg }}
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: 123,
+    }
+  },
+}
+</script>
+<style lang="less">
+.box {
+  width: 100px;
+  height: 100px;
+  background-color: #cdc;
+}
+</style>
